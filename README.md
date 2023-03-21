757a6e5bed13df784f4af5d3483224dad430332a
jtymoszczuk <joetymo@gmail.com>
2023-03-21 02:10:16 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cpfp-calc
---
+++ cpfp-calc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "This estimates how many sats/vbyte you need to bump your child tx to get your desired parent fee"
+
+#x= targetChildFee
+#x = (sat/vB_effective * (vB_parent + vB_child) - current_parent_fee) / vB_child
+
+read -p "Enter the total amount of sats your current(parent) tx cost? " currentFee
+
+read -p "What fee do you want your tx to be in sats/vbyte? " effectiveFee
+
+read -p "Enter Virtual size of Parent? " vBParent
+
+read -p "Enter Virtual size of child? " vBChild
+
+x=$(echo "scale=2; (($effectiveFee * ($vBParent + $vBChild)) - $currentFee) / $vBChild" | bc)
+
+echo -e "Your Child tx needs to be \033[32m$x\033[0m sats/vByte for your parent tx to be \033[32m$effectiveFee\033[0m sats/vByte"
